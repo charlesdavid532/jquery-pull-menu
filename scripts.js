01d00dbc7649daa011d3a1439b1c7e66652321e6
charles531 <charlesdavid531@gmail.com>
2014-12-05 09:37:53 +0530

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,23 @@
+﻿$(function () {
+    
+
+    function render() {
+        _bindEvents();
+    }
+
+    function _bindEvents() {
+        $('.image-container').on('click', _onImageClick);
+    }
+
+    function _onImageClick(event) {
+        console.log('image clicked');
+        var $a = _getBigImageDiv();
+    }
+
+    function _getBigImageDiv() {
+        var $bigImage = $('<div  id="big-image-container" class="big-image-container"></div>');
+        return $bigImage;
+    }
+
+    render();
+});
